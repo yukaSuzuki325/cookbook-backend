7aeb89c335e1f840c54991012b83928451f04eb6
Yuka Suzuki <yuka.kurihara821@gmail.com>
2025-02-11 14:39:16 +0000

    Add public frontend domain to cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://amazingcookbook.netlify.app'],
     credentials: true, // Allow cookies to be sent
   })
 );
