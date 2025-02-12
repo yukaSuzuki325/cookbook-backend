deb572b04116de6db92681e3165ccacc6d95214f
Yuka Suzuki <yuka.kurihara821@gmail.com>
2025-02-12 08:57:46 +0000

    Refactor generateTokens function

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -9,8 +9,8 @@ const generateToken = (res, userId) => {
   //Save the token in cookie
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'strict',
+    secure: process.env.NODE_ENV !== 'development', // Only use secure cookies in production
+    sameSite: 'None', // Allows cross-site cookies - set 'None' if using different domains for back/frontend
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
